cf64dc8429a99b723f72774a16cc11a99df7b734
Rob <Rob@vandersluis.net>
2025-04-13 22:07:07 +0200

    Feature: Add new keymap for Keychron K6 Pro with RGB support and tap dance functionality

---
+++ keyboards/keychron/k6_pro/ansi/rgb/keymaps/rob/keymap.c
@@ -0,0 +1,122 @@
+/* Copyright 2021 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+// clang-format off
+enum layers{
+  MAC_BASE,
+  WIN_BASE,
+  MAC_FN1,
+  WIN_FN1,
+  FN2,
+  FN3,
+};
+
+enum {
+  TD_PGUP_PGDN = 0,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+[MAC_BASE] = LAYOUT_ansi_68(
+     KC_GRV,                KC_F1, KC_F2,  KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,  KC_F9,   KC_F10,   KC_F11,   KC_F12,   RGB_TOG, RGB_MOD,
+     MT(MOD_LALT, KC_TAB),  KC_Q,  KC_W,   KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,   KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS, KC_HOME,
+     MT(MOD_LCTL, KC_ESC),  KC_A,  KC_S,   KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,   KC_L,    KC_SCLN,  KC_QUOT,  MT(MOD_RGUI, KC_ENT),  KC_PGUP,
+     MT(MOD_LSFT, KC_DEL),  KC_Z,  KC_X,   KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,KC_DOT,  KC_SLSH,  MT(MOD_RSFT, KC_BSPC),  KC_UP,    KC_PGDN,
+     KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,   MO(FN2),MO(MAC_FN1),TG(FN2), KC_LEFT,  KC_DOWN, KC_RGHT),
+
+[WIN_BASE] = LAYOUT_ansi_68(
+     KC_GRV,                KC_F1,  KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,  KC_F7, KC_F8,  KC_F9,  KC_F10,     KC_F11,  KC_F12,  RGB_TOG, RGB_MOD,
+     MT(MOD_LALT, KC_TAB),  KC_Q,   KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS, KC_HOME,
+     MT(MOD_LCTL, KC_ESC),  KC_A,   KC_S,  KC_D,  KC_F,  KC_G,  KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,  KC_QUOT,  MT(MOD_RALT, KC_ENT),  KC_PGUP,
+     MT(MOD_LSFT, KC_DEL),  KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,  KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,  MT(MOD_RSFT, KC_BSPC),  KC_UP,    KC_PGDN,
+     KC_LALT,  KC_LGUI,  KC_LCTL,                                KC_SPC,                       MO(FN2), MO(WIN_FN1),TG(FN2), KC_LEFT,  KC_DOWN, KC_RGHT),
+
+[MAC_FN1] = LAYOUT_ansi_68(
+     KC_GRV,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  RGB_TOG,
+    KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,
+     KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+
+[WIN_FN1] = LAYOUT_ansi_68(
+     KC_GRV,   KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  RGB_TOG,
+     KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+     KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+
+[FN2] = LAYOUT_ansi_68(
+
+     KC_NO,    KC_KP_1,  KC_KP_2,  KC_KP_3,  KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_KP_7,  KC_KP_8,  KC_KP_9,  KC_KP_0,   KC_MINUS,    KC_EQUAL,    RGB_TOG, RGB_MOD,
+     KC_TRNS,  KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR,  KC_PERCENT,  KC_CIRCUMFLEX,  KC_AMPERSAND,  KC_ASTERISK,  KC_LEFT_PAREN,  KC_RIGHT_PAREN,  KC_UNDERSCORE,  KC_PLUS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  TD(TD_PGUP_PGDN),  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT,  KC_END,   KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_VAI,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_HUD,  RGB_VAD  ,  RGB_HUI),
+
+[FN3] = LAYOUT_ansi_68(
+     KC_TILD,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+  [TD_PGUP_PGDN] = ACTION_TAP_DANCE_DOUBLE(KC_PGDN, KC_PGUP),
+};
+
+static uint8_t last_hue = 0;
+static uint8_t last_sat = 0;
+static uint8_t last_val = 0;
+
+void set_rgb(uint8_t h, uint8_t s, uint8_t v) {
+    if (h != last_hue || s != last_sat || v != last_val) {
+        rgb_matrix_sethsv_noeeprom(h, s, v);
+        last_hue = h;
+        last_sat = s;
+        last_val = v;
+    }
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+        case FN2:
+            set_rgb(0, 255, 155); // rood
+            break;
+        case FN3:
+            set_rgb(0, 255, 255); // groen
+            break;
+        case MAC_FN1:
+        case WIN_FN1:
+            set_rgb(200, 255, 255); // paars
+            break;
+        case 0:
+        default:
+            set_rgb(0, 0, 255); // wit/blauwachtig
+            break;
+    }
+    return state;
+}
+
+void keyboard_post_init_user(void) {
+    rgb_matrix_enable();
+    rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR); // vaste kleur
+    rgb_matrix_sethsv(0, 0, 100); // wit, hue 0, sat 0, value 100
+}
+
+
---
+++ keyboards/keychron/k6_pro/ansi/rgb/keymaps/rob/rules.mk
@@ -0,0 +1,7 @@
+#
+# Enable VIA support
+#
+#VIA_ENABLE = yes
+#LTO_ENABLE = yes
+TAP_DANCE_ENABLE=yes
+
