31a77ccc4302a73d48070166f08bd66d5e614078
Rob <Rob@vandersluis.net>
2025-04-16 19:36:36 +0200

    Refactor: Update keymap layout for Keychron K6 Pro with improved modifier key handling and removed commented code.

--- keyboards/keychron/k6_pro/ansi/rgb/keymaps/rob/keymap.c
+++ keyboards/keychron/k6_pro/ansi/rgb/keymaps/rob/keymap.c
@@ -31,27 +31,25 @@ enum {
   TD_CTRLZ,
   TD_ESC_CAPS,
 };
-/*
-DdgfdfsISFJsgF* fsad sad fasdf jkkj fdas j
-*/
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [MAC_BASE] = LAYOUT_ansi_68(
      KC_GRV,                KC_1,  KC_2,   KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,   KC_9,    KC_0,     KC_MINS,  KC_EQL, LALT(KC_BSPC) ,         RGB_TOG,
-     KC_TAB,       KC_Q,  KC_W,   KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,   KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,               KC_VOLU,
-     MT(MOD_LCTL, KC_ESC),  KC_A,  KC_S,   KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,   KC_L,    KC_SCLN,  KC_QUOT,       MT(MOD_RGUI, KC_ENT),      KC_VOLD,
+     MT(MOD_LALT, KC_TAB),  KC_Q,  KC_W,   KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,   KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,               KC_VOLU,
+     TD(TD_ESC_CAPS),       KC_A,  KC_S,   KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,   KC_L,    KC_SCLN,  KC_QUOT,       MT(MOD_RGUI, KC_ENT),      KC_VOLD,
      MT(MOD_LSFT, KC_DEL),  KC_Z,  KC_X,   KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,KC_DOT,  MT(MOD_RSFT,KC_SLSH),MT(MOD_RSFT, KC_BSPC),  KC_UP, KC_MPLY,
      KC_LCTL,  KC_LOPTN, KC_LCMMD, KC_SPC,                                            MO(FN2),MO(MAC_FN1),TG(FN2),          KC_LEFT,    KC_DOWN,   KC_RGHT),
 
 [WIN_BASE] = LAYOUT_ansi_68(
-     KC_GRV,                KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,   KC_7,  KC_8,   KC_9,   KC_0,     KC_MINS,    KC_EQL,  LCTL(KC_BSPC) ,       RGB_TOG,
-     MT(MOD_LALT, KC_TAB),  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,             KC_VOLU,
+     KC_GRV,                KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,   KC_7,  KC_8,   KC_9,   KC_0,     KC_MINS,    KC_EQL,      LCTL(KC_BSPC),    RGB_TOG,
+     MT(MOD_LALT, KC_TAB),  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,     KC_LBRC,    KC_RBRC,     KC_BSLS,          KC_VOLU,
      TD(TD_ESC_CAPS),       KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,  KC_QUOT,    MT(MOD_RALT, KC_ENT),          KC_VOLD,
      MT(MOD_LSFT, KC_DEL),  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,   KC_M,  KC_COMM,KC_DOT, MT(MOD_RSFT,KC_SLSH), MT(MOD_RSFT, KC_BSPC), KC_UP,  KC_MPLY,
-     KC_LALT ,  KC_LGUI,  TD(TD_CTRLZ),  KC_SPC,                                              MO(FN2), MO(WIN_FN1),TG(FN3),       KC_LEFT,  KC_DOWN,KC_RGHT),
+     KC_LALT ,  KC_LGUI,  TD(TD_CTRLZ),  KC_SPC,                                              MO(FN2), MO(WIN_FN1),TG(FN3),      KC_LEFT,  KC_DOWN,KC_RGHT),
 
 [MAC_FN1] = LAYOUT_ansi_68(
-     KC_GRV,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  RGB_TOG,
+     KC_TRNS,  KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  RGB_TOG,
      KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,
      KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
@@ -66,17 +64,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [FN2] = LAYOUT_ansi_68(
 
-     KC_NO,    KC_F1,       KC_F2,  KC_F3,    KC_F4,      KC_F5,       KC_F6,          KC_F7,         KC_F8,  KC_F9,  KC_F10,   KC_F11,    KC_F12,    KC_F13,   KC_F14,
-     KC_TRNS,  KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR,  KC_PERCENT,  KC_CIRCUMFLEX,  KC_AMPERSAND,  KC_ASTERISK,  KC_LEFT_PAREN,  KC_RIGHT_PAREN,  KC_UNDERSCORE,  KC_PLUS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  TD(TD_PGUP_PGDN),  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT,  KC_END,   KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_VAI,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_HUD,  RGB_VAD  ,  RGB_HUI),
+     KC_NO,    KC_F1,       KC_F2,    KC_F3,    KC_F4,             KC_F5,       KC_F6,          KC_F7,         KC_F8,       KC_F9,          KC_F10,         KC_F11,        KC_F12,  KC_F13,   KC_F14,
+     KC_TRNS,  KC_EXCLAIM,  KC_AT,    KC_HASH,  KC_DOLLAR,         KC_PERCENT,  KC_CIRCUMFLEX,  KC_AMPERSAND,  KC_ASTERISK, KC_LEFT_PAREN,  KC_RIGHT_PAREN, KC_UNDERSCORE, KC_PLUS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,     KC_TRNS,  KC_TRNS,  TD(TD_PGUP_PGDN),  KC_HOME,     KC_LEFT,        KC_DOWN,       KC_UP,       KC_RGHT,        KC_END,         KC_TRNS,            KC_TRNS,      KC_TRNS,
+     KC_TRNS,  KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL, KC_TRNS,     KC_TRNS,        KC_TRNS,       KC_TRNS,     KC_TRNS,                                    KC_TRNS,    RGB_VAI,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,     KC_TRNS,                               KC_TRNS,                                                       KC_TRNS,       KC_TRNS,     KC_TRNS,      RGB_HUD,RGB_VAD,  RGB_HUI),
 
 [FN3] = LAYOUT_ansi_68(
-     KC_NO,    KC_KP_1,  KC_KP_2,  KC_KP_3,  KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_KP_7,  KC_KP_8,  KC_KP_9,  KC_KP_0,  KC_MINUS, KC_EQUAL, KC_BSPC, KC_DEL,
+     KC_NO,    KC_KP_1,  KC_KP_2,  KC_KP_3,  KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_KP_7,  KC_KP_8,  KC_KP_9,  KC_KP_0,  KC_MINUS, KC_EQUAL, KC_BSPC,  KC_DEL,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 };
 
