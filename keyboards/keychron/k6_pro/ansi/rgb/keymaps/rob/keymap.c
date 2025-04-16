56d14ca664ac0dba86e5a16a52d2160546675a43
Rob <Rob@vandersluis.net>
2025-04-16 12:36:57 +0200

    Feature: Add tap dance functionality for Ctrl+Z and enhanced ESC/CAPS key behavior in keymap.c

--- keyboards/keychron/k6_pro/ansi/rgb/keymaps/rob/keymap.c
+++ keyboards/keychron/k6_pro/ansi/rgb/keymaps/rob/keymap.c
@@ -28,23 +28,27 @@ enum layers{
 
 enum {
   TD_PGUP_PGDN = 0,
+  TD_CTRLZ,
+  TD_ESC_CAPS,
 };
-
+/*
+DdgfdfsISFJsgF* fsad sad fasdf jkkj fdas j
+*/
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [MAC_BASE] = LAYOUT_ansi_68(
-     KC_GRV,                KC_1,  KC_2,   KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,   KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,               RGB_TOG,
-     KC_TAB,                KC_Q,  KC_W,   KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,   KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,               KC_VOLU,
+     KC_GRV,                KC_1,  KC_2,   KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,   KC_9,    KC_0,     KC_MINS,  KC_EQL, LALT(KC_BSPC) ,         RGB_TOG,
+     KC_TAB,       KC_Q,  KC_W,   KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,   KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,               KC_VOLU,
      MT(MOD_LCTL, KC_ESC),  KC_A,  KC_S,   KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,   KC_L,    KC_SCLN,  KC_QUOT,       MT(MOD_RGUI, KC_ENT),      KC_VOLD,
      MT(MOD_LSFT, KC_DEL),  KC_Z,  KC_X,   KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,KC_DOT,  MT(MOD_RSFT,KC_SLSH),MT(MOD_RSFT, KC_BSPC),  KC_UP, KC_MPLY,
      KC_LCTL,  KC_LOPTN, KC_LCMMD, KC_SPC,                                            MO(FN2),MO(MAC_FN1),TG(FN2),          KC_LEFT,    KC_DOWN,   KC_RGHT),
 
 [WIN_BASE] = LAYOUT_ansi_68(
-     KC_GRV,                KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,   KC_7,  KC_8,   KC_9,   KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,             RGB_TOG,
+     KC_GRV,                KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,   KC_7,  KC_8,   KC_9,   KC_0,     KC_MINS,    KC_EQL,  LCTL(KC_BSPC) ,       RGB_TOG,
      MT(MOD_LALT, KC_TAB),  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,             KC_VOLU,
-     MT(MOD_LCTL, KC_ESC),  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,  KC_QUOT,    MT(MOD_RALT, KC_ENT),          KC_VOLD,
+     TD(TD_ESC_CAPS),       KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,  KC_QUOT,    MT(MOD_RALT, KC_ENT),          KC_VOLD,
      MT(MOD_LSFT, KC_DEL),  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,   KC_M,  KC_COMM,KC_DOT, MT(MOD_RSFT,KC_SLSH), MT(MOD_RSFT, KC_BSPC), KC_UP,  KC_MPLY,
-    LT(FN3, KC_LALT) ,  KC_LGUI,  KC_LCTL,  KC_SPC,                                              MO(FN2), MO(WIN_FN1),TG(FN3),       KC_LEFT,    KC_DOWN,   KC_RGHT),
+     KC_LALT ,  KC_LGUI,  TD(TD_CTRLZ),  KC_SPC,                                              MO(FN2), MO(WIN_FN1),TG(FN3),       KC_LEFT,  KC_DOWN,KC_RGHT),
 
 [MAC_FN1] = LAYOUT_ansi_68(
      KC_GRV,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  RGB_TOG,
@@ -64,21 +68,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      KC_NO,    KC_F1,       KC_F2,  KC_F3,    KC_F4,      KC_F5,       KC_F6,          KC_F7,         KC_F8,  KC_F9,  KC_F10,   KC_F11,    KC_F12,    KC_F13,   KC_F14,
      KC_TRNS,  KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR,  KC_PERCENT,  KC_CIRCUMFLEX,  KC_AMPERSAND,  KC_ASTERISK,  KC_LEFT_PAREN,  KC_RIGHT_PAREN,  KC_UNDERSCORE,  KC_PLUS,  KC_TRNS,  KC_TRNS,
-     KC_CAPS ,  KC_TRNS,  KC_TRNS,  KC_TRNS,  TD(TD_PGUP_PGDN),  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT,  KC_END,   KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  TD(TD_PGUP_PGDN),  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT,  KC_END,   KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_VAI,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_HUD,  RGB_VAD  ,  RGB_HUI),
 
 [FN3] = LAYOUT_ansi_68(
      KC_NO,    KC_KP_1,  KC_KP_2,  KC_KP_3,  KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_KP_7,  KC_KP_8,  KC_KP_9,  KC_KP_0,  KC_MINUS, KC_EQUAL, KC_BSPC, KC_DEL,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_CAPS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 };
 
+bool ctrlz_held = false;
+
+void td_ctrlz_finished(tap_dance_state_t *state, void *user_data) {
+  if (state->pressed && !state->interrupted && state->count == 1) {
+    ctrlz_held = true;
+    register_code(KC_LCTL);
+  } else if (state->count == 1) {
+    // Tap = Ctrl+Z
+    register_code(KC_LCTL);
+    tap_code(KC_Z);
+    unregister_code(KC_LCTL);
+  }
+}
+
+void td_ctrlz_reset(tap_dance_state_t *state, void *user_data) {
+  if (ctrlz_held) {
+    ctrlz_held = false;
+    unregister_code(KC_LCTL);
+  }
+}
+
 tap_dance_action_t tap_dance_actions[] = {
-  [TD_PGUP_PGDN] = ACTION_TAP_DANCE_DOUBLE(KC_PGDN, KC_PGUP),
+    [TD_CTRLZ] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_ctrlz_finished, td_ctrlz_reset),
+    [TD_PGUP_PGDN] = ACTION_TAP_DANCE_DOUBLE(KC_PGDN, KC_PGUP),
+    [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
 };
+
 
 static uint8_t last_hue = 0;
 static uint8_t last_sat = 0;
